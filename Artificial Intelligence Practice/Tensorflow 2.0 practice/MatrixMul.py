ef5f43bf14768a1b2f9d1db718b4c4802d28f92f
printfLcc <1295470449@qq.com>
2020-01-24 20:36:34 +0800

    Update MatrixMul.py

--- Artificial Intelligence Practice/Tensorflow 2.0 practice/MatrixMul.py
+++ Artificial Intelligence Practice/Tensorflow 2.0 practice/MatrixMul.py
@@ -1,34 +1,25 @@
-import tensorflow as tf
+import torch
 import timeit
 
-# 创建在 CPU 上运算的 2 个矩阵
-with tf.device('/cpu:0'):
-    cpu_a = tf.random.normal([10000, 10000])
-    cpu_b = tf.random.normal([10000, 10000])
-    print(cpu_a.device, cpu_b.device)
+a = torch.randn(10000, 10000)
+b = torch.randn(10000, 10000)
 
-# 创建使用 GPU 运算的 2 个矩阵
-with tf.device('/gpu:0'):
-    gpu_a = tf.random.normal([10000, 10000])
-    gpu_b = tf.random.normal([10000, 10000])
-    print(gpu_a.device, gpu_b.device)
-
-
+c = torch.randn(10000, 10000).cuda()
+d = torch.randn(10000, 10000).cuda()
 
 def cpu_run():
-    with tf.device('/cpu:0'):
-        c = tf.matmul(cpu_a, cpu_b)
-        return c
+    a1 = torch.matmul(a, b)
+    return a1
+
 def gpu_run():
-    with tf.device('/gpu:0'):
-        c = tf.matmul(gpu_a, gpu_b)
-        return c
-        # 第一次计算需要热身，避免将初始化阶段时间结算在内
+    a2 = torch.matmul(c, d).cuda()
+    return a2
+
 
 cpu_time = timeit.timeit(cpu_run, number=10)
 gpu_time = timeit.timeit(gpu_run, number=10)
-print('warmup: CPU_Time: ', str(cpu_time) + ' GPU_Time' + str(gpu_time))
+print('warmup: CPU_Time: ', str(cpu_time) + 's GPU_Time: ' + str(gpu_time))
 # 正式计算 10 次，取平均时间
 cpu_time = timeit.timeit(cpu_run, number=10)
 gpu_time = timeit.timeit(gpu_run, number=10)
-print('run time: CPU_Time: ', str(cpu_time) + ' GPU_Time' + str(gpu_time))
+print('run time: CPU_Time: ', str(cpu_time) + 's GPU_Time: ' + str(gpu_time))
